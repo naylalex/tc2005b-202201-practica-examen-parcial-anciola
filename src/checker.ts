65ec04d3fd156039661b9afc9efef2bc983ab578
anciola <90790959+anciola@users.noreply.github.com>
2022-03-16 00:58:25 +0000

    ye

--- src/checker.ts
+++ src/checker.ts
@@ -1,5 +1,5 @@
 function isPalindrome(text: string) {
 for 
 
-  * isPalindrome es `true` si cada uno de sus caracteres de izquierda a derecha (índice 0 a indíce length -1) corresponde al caracter del texto en el orden inverso, de derecha a izquiera (índice length - 1 a índice 0). Ignorar la diferencia entre mayúsculas y minúsculas en la comparación.
+  "* isPalindrome es `true` si cada uno de sus caracteres de izquierda a derecha (índice 0 a indíce length -1) corresponde al caracter del texto en el orden inverso, de derecha a izquiera (índice length - 1 a índice 0). Ignorar la diferencia entre mayúsculas y minúsculas en la comparación."
 }
